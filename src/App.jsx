05d747034fd6383e85fd00c11852c5d7b88dbb5b
adarsh sharma <37371998+adarshsharma27@users.noreply.github.com>
2023-10-27 21:02:43 +0530

    UI Changes for Better UI experience

--- src/App.jsx
+++ src/App.jsx
@@ -59,6 +59,7 @@ function App() {
               placeholder="Password"
               value={password}
               ref={ref}
+              disabled
             />
             {copyPassword ? (
               <FaRegCopy className="copy-clipboard" />
